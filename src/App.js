cb52aec40029a0a5d511a4d97025939ae33fd59b
Rodrigo <rodridega@gmail.com>
2022-02-15 18:53:53 -0300

    add build

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
-=======
-import React, { useState, useEffect } from 'react';
->>>>>>> 661a474589108b655d511791c0f478c4ed4aa12b
 import './App.css';
 import Card from './components/Card';
 import Formulario from './components/Formulario';
